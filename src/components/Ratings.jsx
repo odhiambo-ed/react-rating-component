9757c8a951a27ec8f53ea97fc176adf1bd1a0181
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-28 20:44:53 +0300

    add container

--- src/components/Ratings.jsx
+++ src/components/Ratings.jsx
@@ -2,7 +2,11 @@
 
 function Ratings() {
   return (
-    <div>Ratings</div>
+      <div className="container-fluid vw-100 vh-100 bg-light d-flex justify-content-center align-items-center">
+          <div className="border border-success rounded-pill py-2 px-5">
+             <h5>Ratings</h5> 
+          </div>
+    </div>
   )
 }
 
