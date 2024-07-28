37b2ab6494e8563d60479ae90231404310f8ab1d
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-28 21:54:13 +0300

    edit rating

--- src/components/Ratings.jsx
+++ src/components/Ratings.jsx
@@ -5,13 +5,13 @@ function Ratings() {
     <div className="container-fluid vw-100 vh-100 bg-light d-flex justify-content-center align-items-center">
       <div className="border border-success rounded-pill py-2 px-5">
         <h5>Ratings</h5>
-        {Array.from({ length: 10 }).map((_, index) => (
-          <>
-                <BsStar
-                    key={index}
-                />
-          </>
-        ))}
+              {Array.from({ length: 10 }).map((_, index) => {
+                  return (
+                    <>
+                      <BsStar key={index} />
+                    </>
+                  );
+              })}
       </div>
     </div>
   );
