78059fc421e3fcaf800cbf9f1dcf45380a2c6fe6
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-28 21:49:02 +0300

    map star

--- src/components/Ratings.jsx
+++ src/components/Ratings.jsx
@@ -1,13 +1,20 @@
-
+import { BsStar } from "react-icons/bs";
 
 function Ratings() {
   return (
-      <div className="container-fluid vw-100 vh-100 bg-light d-flex justify-content-center align-items-center">
-          <div className="border border-success rounded-pill py-2 px-5">
-             <h5>Ratings</h5> 
-          </div>
+    <div className="container-fluid vw-100 vh-100 bg-light d-flex justify-content-center align-items-center">
+      <div className="border border-success rounded-pill py-2 px-5">
+        <h5>Ratings</h5>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <>
+                <BsStar
+                    key={index}
+                />
+          </>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Ratings
+export default Ratings;
