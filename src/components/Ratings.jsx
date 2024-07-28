9e117a043ba8c8e2d9877440a5e6b8daf874ff0c
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-28 22:11:57 +0300

    add rating css file

--- src/components/Ratings.jsx
+++ src/components/Ratings.jsx
@@ -1,6 +1,10 @@
 import { BsStar } from "react-icons/bs";
+import './Ratings.css'
 
 function Ratings() {
+    const handleClick = (index) => {
+        console.log(index)
+    }
   return (
     <div className="container-fluid vw-100 vh-100 bg-light d-flex justify-content-center align-items-center">
       <div className="border border-success rounded-pill py-2 px-5">
@@ -8,7 +12,11 @@ function Ratings() {
               {Array.from({ length: 10 }).map((_, index) => {
                   return (
                     <>
-                      <BsStar key={index} />
+                          <BsStar
+                              key={index}
+                              className="star"
+                              onClick={() => handleClick(index)}
+                          />
                     </>
                   );
               })}
